34ba52e77bfa2fadbf85a458d361b112c7ef64d2
Vinicius-Marcondes <viniciusmarc_@hotmail.com>
2020-05-30 03:27:52 -0300

    Added migrations and updated routes

--- API/views/person.py
+++ API/views/person.py
@@ -6,7 +6,7 @@ from API.schemas.serealizer import PersonSchema
 bp_person = Blueprint('person', __name__)
 
 
-@bp_person.route('/insert', methods=['POST'])
+@bp_person.route('/api/insert', methods=['POST'])
 def insert():
     ps = PersonSchema()
     person = ps.load(request.json)
@@ -16,17 +16,17 @@ def insert():
     return ps.jsonify(person), 201
 
 
-@bp_person.route('/show', methods=['GET'])
+@bp_person.route('/api/show', methods=['GET'])
 def show():
     result = Person.query.all()
     return PersonSchema(many=True).jsonify(result), 200
 
 
-@bp_person.route('/update', methods=['POST'])
+@bp_person.route('/api/update', methods=['POST'])
 def update():
     ...
 
 
-@bp_person.route('/delete', methods=['GET'])
+@bp_person.route('/api/delete', methods=['GET'])
 def delete():
     ...

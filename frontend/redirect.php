a4a7d72536406509e1c2268057aea1bd55b46f56
Matheus Freitas <matheusfreitasbsi@gmail.com>
2020-06-02 17:38:22 -0300

    Inserindo página de redirecionamento PHP

---
+++ frontend/redirect.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Cadastro Concluído</title>
+    <link rel="stylesheet" href="src/style.css">
+  </head>
+  <body>
+    <div id="external">
+
+  </body>
+</html>
